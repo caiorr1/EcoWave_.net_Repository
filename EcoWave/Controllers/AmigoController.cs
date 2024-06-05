c0821d8e07790bba85f4ebf453237831e5238d6b
caiorr1 <caiorrodrigues2004@gmail.com>
2024-06-05 16:43:38 -0300

    add

--- EcoWave/Controllers/AmigoController.cs
+++ EcoWave/Controllers/AmigoController.cs
@@ -1,81 +1,170 @@
-﻿using EcoWave.Models;
-using EcoWave.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EcoWave.Data;
+using EcoWave.Models;
 
 namespace EcoWave.Controllers
 {
-    public class AmigosController : Controller
+    public class AmigoController : Controller
     {
-        private readonly IRepository<Amigo> _repository;
+        private readonly ApplicationDbContext _context;
 
-        public AmigosController(IRepository<Amigo> repository)
+        public AmigoController(ApplicationDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
 
+        // GET: Amigo
         public async Task<IActionResult> Index()
         {
-            return View(await _repository.GetAll());
+            var applicationDbContext = _context.Amigos.Include(a => a.AmigoUsuario).Include(a => a.Usuario);
+            return View(await applicationDbContext.ToListAsync());
         }
 
+        // GET: Amigo/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) return NotFound();
-            var amigo = await _repository.GetById(id.Value);
-            if (amigo == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amigo = await _context.Amigos
+                .Include(a => a.AmigoUsuario)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.UsuarioId == id);
+            if (amigo == null)
+            {
+                return NotFound();
+            }
+
             return View(amigo);
         }
 
-        public IActionResult Create() => View();
+        // GET: Amigo/Create
+        public IActionResult Create()
+        {
+            ViewData["AmigoId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email");
+            return View();
+        }
 
+        // POST: Amigo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("UsuarioId,AmigoId,DataAmizade")] Amigo amigo)
         {
             if (ModelState.IsValid)
             {
-                await _repository.Add(amigo);
+                _context.Add(amigo);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["AmigoId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.AmigoId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.UsuarioId);
             return View(amigo);
         }
 
+        // GET: Amigo/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) return NotFound();
-            var amigo = await _repository.GetById(id.Value);
-            if (amigo == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amigo = await _context.Amigos.FindAsync(id);
+            if (amigo == null)
+            {
+                return NotFound();
+            }
+            ViewData["AmigoId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.AmigoId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.UsuarioId);
             return View(amigo);
         }
 
+        // POST: Amigo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("UsuarioId,AmigoId,DataAmizade")] Amigo amigo)
         {
-            if (id != amigo.UsuarioId) return NotFound(); // Ajustar conforme necessário
+            if (id != amigo.UsuarioId)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
-                await _repository.Update(amigo);
+                try
+                {
+                    _context.Update(amigo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AmigoExists(amigo.UsuarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["AmigoId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.AmigoId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", amigo.UsuarioId);
             return View(amigo);
         }
 
+        // GET: Amigo/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null) return NotFound();
-            var amigo = await _repository.GetById(id.Value);
-            if (amigo == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amigo = await _context.Amigos
+                .Include(a => a.AmigoUsuario)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.UsuarioId == id);
+            if (amigo == null)
+            {
+                return NotFound();
+            }
+
             return View(amigo);
         }
 
+        // POST: Amigo/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            await _repository.Delete(id);
+            var amigo = await _context.Amigos.FindAsync(id);
+            if (amigo != null)
+            {
+                _context.Amigos.Remove(amigo);
+            }
+
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool AmigoExists(int id)
+        {
+            return _context.Amigos.Any(e => e.UsuarioId == id);
         }
     }
 }
--- EcoWave/Controllers/ConfiguracaoController.cs
+++ EcoWave/Controllers/ConfiguracaoController.cs
@@ -1,81 +1,164 @@
-﻿using EcoWave.Models;
-using EcoWave.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EcoWave.Data;
+using EcoWave.Models;
 
 namespace EcoWave.Controllers
 {
-    public class ConfiguracoesController : Controller
+    public class ConfiguracaoController : Controller
     {
-        private readonly IRepository<Configuracao> _repository;
+        private readonly ApplicationDbContext _context;
 
-        public ConfiguracoesController(IRepository<Configuracao> repository)
+        public ConfiguracaoController(ApplicationDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
 
+        // GET: Configuracao
         public async Task<IActionResult> Index()
         {
-            return View(await _repository.GetAll());
+            var applicationDbContext = _context.Configuracoes.Include(c => c.Usuario);
+            return View(await applicationDbContext.ToListAsync());
         }
 
+        // GET: Configuracao/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) return NotFound();
-            var configuracao = await _repository.GetById(id.Value);
-            if (configuracao == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var configuracao = await _context.Configuracoes
+                .Include(c => c.Usuario)
+                .FirstOrDefaultAsync(m => m.ConfiguracaoId == id);
+            if (configuracao == null)
+            {
+                return NotFound();
+            }
+
             return View(configuracao);
         }
 
-        public IActionResult Create() => View();
+        // GET: Configuracao/Create
+        public IActionResult Create()
+        {
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email");
+            return View();
+        }
 
+        // POST: Configuracao/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ConfiguracaoId,UsuarioId,NomeConfiguracao,ValorConfiguracao")] Configuracao configuracao)
         {
             if (ModelState.IsValid)
             {
-                await _repository.Add(configuracao);
+                _context.Add(configuracao);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", configuracao.UsuarioId);
             return View(configuracao);
         }
 
+        // GET: Configuracao/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) return NotFound();
-            var configuracao = await _repository.GetById(id.Value);
-            if (configuracao == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var configuracao = await _context.Configuracoes.FindAsync(id);
+            if (configuracao == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", configuracao.UsuarioId);
             return View(configuracao);
         }
 
+        // POST: Configuracao/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ConfiguracaoId,UsuarioId,NomeConfiguracao,ValorConfiguracao")] Configuracao configuracao)
         {
-            if (id != configuracao.ConfiguracaoId) return NotFound();
+            if (id != configuracao.ConfiguracaoId)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
-                await _repository.Update(configuracao);
+                try
+                {
+                    _context.Update(configuracao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConfiguracaoExists(configuracao.ConfiguracaoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Email", configuracao.UsuarioId);
             return View(configuracao);
         }
 
+        // GET: Configuracao/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null) return NotFound();
-            var configuracao = await _repository.GetById(id.Value);
-            if (configuracao == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var configuracao = await _context.Configuracoes
+                .Include(c => c.Usuario)
+                .FirstOrDefaultAsync(m => m.ConfiguracaoId == id);
+            if (configuracao == null)
+            {
+                return NotFound();
+            }
+
             return View(configuracao);
         }
 
+        // POST: Configuracao/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            await _repository.Delete(id);
+            var configuracao = await _context.Configuracoes.FindAsync(id);
+            if (configuracao != null)
+            {
+                _context.Configuracoes.Remove(configuracao);
+            }
+
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConfiguracaoExists(int id)
+        {
+            return _context.Configuracoes.Any(e => e.ConfiguracaoId == id);
         }
     }
 }
